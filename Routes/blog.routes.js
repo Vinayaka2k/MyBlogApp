f8f407f86f22e522293878bb91f06779b0f05b7b
Vinayaka Hegde <vinayaka.h@lsq.net>
2022-10-19 23:32:15 +0530

    completed blog and auth router

---
+++ Routes/blog.routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router()
+const {apiGetBlogs, apiGetBlogById, apiPushBlog, apiGetOneBlog, apiDeleteBlog, apiEditBlog, apiGetBlogByUser} = require("../Controllers/blog.controller")
+const {isAuthenticated} = require("../Services/verification.service")
+
+router.get("/blogs", apiGetBlogs)
+router.get("/blogs/latest", apiGetOneBlog)
+router.post("/blogs", isAuthenticated, apiGetBlogByUser)
+router.post("/post", isAuthenticated, apiPushBlog)
+router.get("/blog/:id", isAuthenticated, apiGetBlogById)
+router.delete("/blog/:id", isAuthenticated, apiDeleteBlog)
+router.patch("/blog/:id", isAuthenticated, apiEditBlog)
+
+module.exports = router
